45e4d38cecf22fd6d895ff84a747585a0e21956e
clementine contat <clementine.contat@gmail.com>
2019-03-30 14:16:08 +0000

    updated the actions syntax

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -1,12 +1,13 @@
 // import flats from '../../data/flats';
-
+export const SET_FLATS = 'SET_FLATS';
+export const SELECT_FLAT = 'SELECT_FLAT';
 
 export function setFlats() {
   return fetch('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json')
     .then(response => response.json())
     .then((data) => {
       return {
-        type: 'SET_FLATS',
+        type: SET_FLATS,
         payload: data
       };
     });
@@ -14,7 +15,7 @@ export function setFlats() {
 
 export function selectFlat(flat) {
   return {
-    type: 'SELECT_FLAT',
+    type: SELECT_FLAT,
     payload: flat
   };
 }
--- src/reducers/flats_reducer.jsx
+++ src/reducers/flats_reducer.jsx
@@ -1,9 +1,11 @@
+import { SET_FLATS } from '../actions';
+
 export default function(state, action) {
   if (state === undefined) {
     return [];
   }
 
-  if (action.type === 'SET_FLATS') {
+  if (action.type === SET_FLATS) {
     return action.payload;
   }
   return state;
